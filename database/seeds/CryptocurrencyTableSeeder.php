5e948974d44207f3408c5e25047132bfeedcd066
D. Robin <dyrda.robin@outlook.fr>
2017-09-16 02:07:14 +0200

    Default timestamp for migrations + code cleanup

--- database/seeds/CryptocurrencyTableSeeder.php
+++ database/seeds/CryptocurrencyTableSeeder.php
@@ -17,85 +17,67 @@ class CryptocurrencyTableSeeder extends Seeder
                 'name'              =>  'Bitcoin',
                 'symbol'            =>  'BTC',
                 'logo_url'          =>  'https://i.imgur.com/5i4e1Vi.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  6,
                 'block_explorer'    =>  'https://blockchain.info',
                 'tx_explorer'       =>  'https://blockchain.info/en/tx/',
-                'uri'               =>  'bitcoin',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'bitcoin'
             ),
             array(
                 'name'              =>  'Litecoin',
                 'symbol'            =>  'LTC',
                 'logo_url'          =>  'https://i.imgur.com/R29q3dD.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  6,
                 'block_explorer'    =>  'https://live.blockcypher.com/ltc',
                 'tx_explorer'       =>  'https://live.blockcypher.com/ltc/tx/',
-                'uri'               =>  'litecoin',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'litecoin'
             ),
             array(
                 'name'              =>  'Dash',
                 'symbol'            =>  'DASH',
                 'logo_url'          =>  'https://i.imgur.com/qiIMHZr.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  6,
                 'block_explorer'    =>  'https://explorer.dash.org',
                 'tx_explorer'       =>  'https://explorer.dash.org/tx/',
-                'uri'               =>  'dash',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'dash'
             ),
             array(
                 'name'              =>  'PIVX',
                 'symbol'            =>  'PIVX',
                 'logo_url'          =>  'https://i.imgur.com/c689IAo.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  6,
                 'block_explorer'    =>  'http://pivxscan.io',
                 'tx_explorer'       =>  'http://pivxscan.io/tx/',
-                'uri'               =>  'pivx',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'pivx'
             ),
             array(
                 'name'              =>  'Nexus',
                 'symbol'            =>  'NXS',
                 'logo_url'          =>  'https://i.imgur.com/Sf3N92M.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  6,
                 'block_explorer'    =>  'http://nxs.efficienthash.com',
                 'tx_explorer'       =>  'http://nxs.efficienthash.com/txes/',
-                'uri'               =>  'nexus',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'nexus'
             ),
             array(
                 'name'              =>  'Dogecoin',
                 'symbol'            =>  'DOGE',
                 'logo_url'          =>  'https://i.imgur.com/e1RS4Hn.png',
-                'last_block_update' =>  Carbon::now()->format('Y-m-d H:i:s'),
                 'last_usd_price'    =>  0,
                 'last_btc_price'    =>  0,
                 'confirmations'     =>  3,
                 'block_explorer'    =>  'https://dogechain.info',
                 'tx_explorer'       =>  'https://dogechain.info/tx/',
-                'uri'               =>  'dogecoin',
-                'created_at'        =>  Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at'        =>  Carbon::now()->format('Y-m-d H:i:s')
+                'uri'               =>  'dogecoin'
             )
         ));
     }

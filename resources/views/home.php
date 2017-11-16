ec32b1ec4624960db7867659fdc02911ec8923f2
Bashilor <dyrda.robin@outlook.fr>
2017-11-16 15:14:19 +0100

    Landing page

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,5 +1,30 @@
-<html>
-    <body>
-        <h1>Hello</h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>AnoPay - Anonymous API for cryptocurrency payement</title>
+
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Barlow+Semi+Condensed" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    <h2>AnoPay</h2>
+    <h5 class="callout">Anonymous API for cryptocurrency payement.</h5>
+
+    <p>To get started, check our <a href="/doc">documentation</a>.</p>
+
+    <div class="changelog">
+        <h3>Changelog :</h3>
+        <div class="current">
+            <h4><span>CURRENT</span> | 11/17/2017 - Version 1.0.0 :</h4>
+            <ul>
+                <li>Official launch.</li>
+            </ul>
+        </div>
+    </div>
+</body>
 </html>
